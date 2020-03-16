04526c52d9027788fc372820f0d5931fc6f81a8e
KiryaPikoff <har4ok@bk.ru>
2020-03-16 18:05:51 +0300

    part_3

---
+++ CarShopBuisnessLogic/HelperModels/PdfCellParameters.cs
@@ -0,0 +1,15 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using MigraDoc.DocumentObjectModel;
+
+namespace CarShopBuisnessLogic.HelperModels
+{
+    class PdfCellParameters
+    {
+        public Cell Cell { get; set; }
+        public string Text { get; set; }
+        public string Style { get; set; }
+        public ParagraphAlignment ParagraphAlignment { get; set; }
+        public Unit BorderWidth { get; set; }
+
+    }
+}
---
+++ CarShopBuisnessLogic/HelperModels/PdfRowParameters.cs
@@ -0,0 +1,14 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using MigraDoc.DocumentObjectModel;
+using System.Collections.Generic;
+
+namespace CarShopBuisnessLogic.HelperModels
+{
+    class PdfRowParameters
+    {
+        public Table Table { get; set; }
+        public List<string> Texts { get; set; }
+        public string Style { get; set; }
+        public ParagraphAlignment ParagraphAlignment { get; set; }
+    }
+}
